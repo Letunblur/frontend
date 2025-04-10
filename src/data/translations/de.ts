f6c078ddd0068efa655f16bb292c85d208f7fba3
Letunblur <hello@letunblur.com>
2025-04-10 20:51:36 +0200

    feat: added de.ts translation file with keys

---
+++ src/data/translations/de.ts
@@ -0,0 +1,7 @@
+export const de: Record<string, string> = {
+  price: "Preis",
+  privateAccess: "Privater Zugang",
+  privateAccessDesc: "Nur für autorisierte Nutzer sichtbar",
+  totalClicks: "Gesamte Klicks",
+  dashboard: "Dashboard",
+}
